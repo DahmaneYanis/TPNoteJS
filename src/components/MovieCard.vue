a019f2af77080f2a48baae6b4d44b56485a2030b
d_yanis <dahmane.yanis.2004@gmail.com>
2024-03-13 18:10:22 +0100

    done

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -6,21 +6,12 @@ const props = defineProps({
     movie: Movie
 })
 console.log(props.movie)
-// let description = descriptionFormate(props.movie.description)
-// component logic
-// declare some reactive state here.
 </script>
 
 <template>
-    <p>Test</p>
-    <!-- <div>
-        <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
-            tag="article" style="max-width: 20rem;" class="mb-2">
-            <b-card-text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-        </b-card>
-    </div> -->
+    <div style="border : 1px solid red;">
+      <li>Titre : {{movie.title}}</li>
+      <li>Description : {{movie.description}}</li>
+      <li>Lien : {{ movie.link }}</li>
+    </div>
 </template>
